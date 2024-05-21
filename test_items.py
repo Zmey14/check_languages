e7ac540e396eecd25e432173578294aec797b164
zmey14 <mishkaneshishka@gmail.com>
2024-05-21 19:48:08 +0400

    fix test file

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 import time
 
 
-def test_button(browser):
+def test_visible_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
