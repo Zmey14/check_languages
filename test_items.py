cc540a7cedfe7771ce9a73c536f8e9748584c0fc
zmey14 <mishkaneshishka@gmail.com>
2024-05-21 18:01:37 +0400

    check languages page

---
+++ test_items.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    time.sleep(5)
+    assert button, "Кнопка отсутствует"
